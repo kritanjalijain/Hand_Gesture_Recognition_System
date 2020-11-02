954d91aacd5f9ac95b8500fbbd25cad1bb10b837
Kritanjali Jain <kritanjalijain@gmail.com>
2020-11-02 13:20:19 +0530

    Update demo.py

--- src/demo.py
+++ src/demo.py
@@ -38,19 +38,19 @@ BGSUBTHRESHOLD = 50
 THRESHOLD = 50
 
 # Gesture Mode variables
-GESTUREMODE = False # Don't ever edit this!
+GESTUREMODE = False 
 GESTURES_RECORDED = [10,10,10,10,10,10,10,10,10,10]
 SONG = 'The Beatles - I Saw Her Standing There'
 ACTIONS_GESTURE_ENCODING = {'fist': 'Play/Unpause', 'five': 'Pause', 'none': 'Do Nothing', 'okay': 'Increase Volume', 'peace': 'Decrease Volume', 'rad': "Load Song", 'straight': "Stop", "thumbs":"NA"}
 
 # Data Collection Mode variables
-DATAMODE = False # Don't ever edit this!
+DATAMODE = False 
 WHERE = "train"
 GESTURE = "okay"
 NUMBERTOCAPTURE = 100
 
 # Testing Predictions of Model Mode variables
-PREDICT = False # Don't ever edit this!
+PREDICT = False
 HISTORIC_PREDICTIONS = [np.ones((1,8)), np.ones((1,8)), np.ones((1,8)), np.ones((1,8)), np.ones((1,8))]
 IMAGEAVERAGING = 5
 
